7bef343d5cff9dda4c392608323400f240663da0
Svenja Bussinger <svenja.bussinger@gmail.com>
2019-05-04 12:58:30 +0200

    Set Up "Tier" Component necessary for "Aufgabe 1"

--- src/components/Tier.vue
+++ src/components/Tier.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="tier">
     <b>Ich bin ein Tier</b>
-    <p>Name: </p>
-    <p>Alter: </p>
-    <p> Futter: </p>
+    <p> {{fullname}}</p>
+    <p>Vorame: {{vorname}}</p>
+    <p>Nachname: {{nachname}}</p>
+    <p>Art: {{art}}</p>
+    <p>Alter: {{alter}}</p>
+    <p> Futter: {{futter}}</p>
+    <img :src="img"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Tier'
+  name: 'Tier',
+  data () {
+    return {
+      vorname: 'Svenjay',
+      nachname: 'Straussiansen',
+      art: 'Strauss',
+      alter: 5,
+      futter: 'Bier',
+      img: 'http://view.stern.de/de/picture/4049001/portrait-tier-vogel-lachen-lustig-strauss-augenblicke-1920.jpg'
+    }
+  },
+  computed: {
+    fullname: function () {
+      return this.vorname + ' ' + this.nachname
+    }
+  }
 }
 </script>
 
 <style scoped>
 .tier {
   border: 1px solid lightgray;
+}
+img {
+  height: 200px;
+  width: 200px;
 }
 </style>
